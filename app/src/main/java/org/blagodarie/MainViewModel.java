3802a9e7f13a660317cb7c0e19545617568479de
sergeGabrus <seg2007@mail.ru>
2020-04-06 18:26:21 +0300

    Изменен приоритет необходимой геолокации. Сделал отображение текущего времени и координат.

--- app/src/main/java/org/blagodarie/MainViewModel.java
+++ app/src/main/java/org/blagodarie/MainViewModel.java
@@ -1,11 +1,16 @@
 package org.blagodarie;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableDouble;
 import androidx.databinding.ObservableField;
 import androidx.lifecycle.ViewModel;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * @author sergeGabrus
@@ -14,7 +19,9 @@ import java.util.Set;
 public final class MainViewModel
         extends ViewModel {
 
-    public ObservableField<String> mLocation = new ObservableField<>("");
+    public ObservableDouble mCurrentLatitude = new ObservableDouble();
+    public ObservableDouble mCurrentLongitude = new ObservableDouble();
+    public ObservableField<String> mCurrentDataTime = new ObservableField<>("");
 
     @NonNull
     private final Set<DisplaySymptom> mSymptoms = new HashSet<>();
@@ -27,6 +34,13 @@ public final class MainViewModel
 
     public MainViewModel () {
         super();
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run () {
+                mCurrentDataTime.set(SimpleDateFormat.getDateTimeInstance().format(new Date()));
+            }
+        },  0, 1000L);
     }
 
     final Set<DisplaySymptom> getSymptoms () {
