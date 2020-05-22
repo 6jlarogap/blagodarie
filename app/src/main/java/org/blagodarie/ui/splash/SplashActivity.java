87bfba0d875625a6c1b6d879aec402eb58469347
sergeGabrus <seg2007@mail.ru>
2020-05-22 15:04:00 +0300

    Сделал блокирующий диалог

--- app/src/main/java/org/blagodarie/ui/splash/SplashActivity.java
+++ app/src/main/java/org/blagodarie/ui/splash/SplashActivity.java
@@ -3,19 +3,32 @@ package org.blagodarie.ui.splash;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.UnderlineSpan;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import org.blagodarie.R;
 import org.blagodarie.authentication.AccountGeneral;
 import org.blagodarie.authentication.Authenticator;
-import org.blagodarie.ui.greeting.GreetingActivity;
-import org.blagodarie.ui.symptoms.SymptomsActivity;
+import org.blagodarie.databinding.TransitionToPlayMarketDialogBinding;
 
 import java.util.Arrays;
 
@@ -31,7 +44,9 @@ public final class SplashActivity
     private AccountManager mAccountManager;
 
     @Override
-    protected final void onCreate (@Nullable final Bundle savedInstanceState) {
+    protected final void onCreate (
+            @Nullable final Bundle savedInstanceState
+    ) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate");
 
@@ -43,6 +58,63 @@ public final class SplashActivity
         super.onResume();
         Log.d(TAG, "onResume");
         chooseAccount();
+    }
+
+    private void showMandatoryUpdateDialog (
+            @NonNull final String incognitoId
+    ) {
+        final String msg = getString(R.string.txt_transition_to_play_market);
+        final Spannable spannable = new SpannableString(msg);
+
+        final ClickableSpan cs1 = new ClickableSpan() {
+            @Override
+            public void onClick (@NonNull View v) {
+                final Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://play.google.com/store/apps/details?id=org.blagodarie"));
+                startActivity(i);
+            }
+        };
+        final ClickableSpan cs2 = new ClickableSpan() {
+            @Override
+            public void onClick (@NonNull View v) {
+                final Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("https://play.google.com/store/apps/details?id=org.blagodarie"));
+                startActivity(i);
+            }
+        };
+        spannable.setSpan(new UnderlineSpan(), 42, 53, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(new UnderlineSpan(), 226, 237, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(cs1, 42, 53, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannable.setSpan(cs2, 226, 237, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        final TransitionToPlayMarketDialogBinding binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.transition_to_play_market_dialog, null, false);
+        binding.setText(spannable);
+        binding.tvText.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.setIncognitoId(incognitoId);
+
+        final AlertDialog alertDialog = new AlertDialog.
+                Builder(this).
+                setView(binding.getRoot()).
+                setPositiveButton(R.string.btn_copy, null).
+                setNegativeButton(R.string.btn_delete, (dialog, which) -> {
+                    Intent intent = new Intent(Intent.ACTION_DELETE);
+                    intent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }).
+                setCancelable(false).
+                create();
+
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
+            final ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            final ClipData clip = ClipData.newPlainText(getString(R.string.txt_incognito_id), incognitoId);
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
+        });
+
     }
 
     private void chooseAccount () {
@@ -64,7 +136,9 @@ public final class SplashActivity
         }
     }
 
-    private void showAccountPicker (@NonNull final Account[] accounts) {
+    private void showAccountPicker (
+            @NonNull final Account[] accounts
+    ) {
         Log.d(TAG, "showAccountPicker accounts=" + Arrays.toString(accounts));
         final String[] names = new String[accounts.length];
         for (int i = 0; i < accounts.length; i++) {
@@ -99,12 +173,17 @@ public final class SplashActivity
                 bundle,
                 this,
                 future -> chooseAccount(),
-                null);
+                null
+        );
     }
 
-    private void toSymptomsActivity (@NonNull final Account account) {
+    private void toSymptomsActivity (
+            @NonNull final Account account
+    ) {
         Log.d(TAG, "toSymptomsActivity account=" + account);
-        startActivity(SymptomsActivity.createSelfIntent(this, account));
-        finish();
+        showMandatoryUpdateDialog(mAccountManager.getUserData(account, AccountGeneral.USER_DATA_INCOGNITO_ID));
+        //startActivity(SymptomsActivity.createSelfIntent(this, account));
+        //finish();
     }
+
 }
